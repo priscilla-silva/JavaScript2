38b91f97aa5790d180821147a7ef59d6c4b96822
Priscilla Silva <priscilla.sg91@gmail.com>
2022-06-15 22:35:24 +0200

    solved 2-5

--- Week3/homework/step2-5.js
+++ Week3/homework/step2-5.js
@@ -4,14 +4,17 @@ function multiplyAll(arr) {
   // eslint-disable-next-line
   let product = 1;
 
-  // Replace this comment and the next line with your code
-  console.log(arr, product);
-
+  for (let i = 0; i < arr.length; i++) {
+    //console.log(arr, product);
+    for (let j = 0; j < arr[i].length; j++){
+      product = product * arr[i][j];
+    }
+    
+  }
   return product;
 }
-
 const result = multiplyAll([[1, 2], [3, 4], [5, 6]]);
 console.log(result); // 720
 
 // Do not change or remove anything below this line
-module.exports = multiplyAll;
+//module.exports = multiplyAll;
