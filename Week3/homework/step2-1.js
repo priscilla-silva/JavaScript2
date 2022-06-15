df2c9ad9c00dd121374c860169cf9e16357c7848
Priscilla Silva <priscilla.sg91@gmail.com>
2022-06-15 10:58:37 +0200

    initial commit

--- Week3/homework/step2-1.js
+++ Week3/homework/step2-1.js
@@ -3,6 +3,7 @@
 function foo(func) {
   // What to do here?
   // Replace this comment and the next line with your code
+  func = "Nice to meet you, Bar."
   console.log(func);
 }
 
@@ -10,7 +11,7 @@ function bar() {
   console.log('Hello, I am bar!');
 }
 
-foo(bar);
+foo(bar());
 
 // Do not change or remove anything below this line
 module.exports = foo;
