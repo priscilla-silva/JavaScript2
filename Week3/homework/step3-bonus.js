fd803945e3c37082f6dd20bc81d7f1513f855d06
Priscilla Silva <priscilla.sg91@gmail.com>
2022-06-16 13:28:11 +0200

    done wth bonus

--- Week3/homework/step3-bonus.js
+++ Week3/homework/step3-bonus.js
@@ -3,12 +3,19 @@
 const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
 
 function makeUnique(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+  let newArr = [... new Set(arr)];
+  return newArr;
+
+  /* Set objects are collections of values. You can iterate through the elements 
+  of a set in insertion order. A value in the Set may ONLY OCCUR ONCE; it is unique 
+  in the Set's collection. 
+  
+  Because each value in the Set has to be unique, the value equality will be checked. 
+  */
 }
 
 const uniqueValues = makeUnique(values);
 console.log(uniqueValues);
 
 // Do not change or remove anything below this line
-module.exports = makeUnique;
+//module.exports = makeUnique;
